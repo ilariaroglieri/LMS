c1e9e778e8849f627127ccc293445d4108da2935
Ilaria Roglieri <ilariaroglieri@gmail.com>
2026-09-21 18:14:20 +0200

    added journal section

--- inc/cpt.php
+++ inc/cpt.php
@@ -29,82 +29,59 @@
   add_action( 'admin_menu', 'rename_default_post_label' );
   add_action( 'init', 'rename_default_post_object' );
 
-  // --- TAX
-  // Register Custom Taxonomy ANNO
-  function project_year_taxonomy() {
+  // Register Custom Post Type
+  function journal_post_type() {
 
     $labels = array(
-      'name'                       => _x( 'Year', 'Taxonomy General Name', 'text_domain' ),
-      'singular_name'              => _x( 'Year', 'Taxonomy Singular Name', 'text_domain' ),
-      'menu_name'                  => __( 'Years', 'text_domain' ),
-      'all_items'                  => __( 'All Items', 'text_domain' ),
-      'parent_item'                => __( 'Parent Item', 'text_domain' ),
-      'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
-      'new_item_name'              => __( 'New Item Name', 'text_domain' ),
-      'add_new_item'               => __( 'Add New Item', 'text_domain' ),
-      'edit_item'                  => __( 'Edit Item', 'text_domain' ),
-      'update_item'                => __( 'Update Item', 'text_domain' ),
-      'view_item'                  => __( 'View Item', 'text_domain' ),
-      'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
-      'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
-      'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
-      'popular_items'              => __( 'Popular Items', 'text_domain' ),
-      'search_items'               => __( 'Search Items', 'text_domain' ),
-      'not_found'                  => __( 'Not Found', 'text_domain' ),
-      'no_terms'                   => __( 'No items', 'text_domain' ),
-      'items_list'                 => __( 'Items list', 'text_domain' ),
-      'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+      'name'                  => _x( 'Entries', 'Post Type General Name', 'text_domain' ),
+      'singular_name'         => _x( 'Entry', 'Post Type Singular Name', 'text_domain' ),
+      'menu_name'             => __( 'Journal', 'text_domain' ),
+      'name_admin_bar'        => __( 'Journal', 'text_domain' ),
+      'archives'              => __( 'Journal', 'text_domain' ),
+      'attributes'            => __( 'Entry Attributes', 'text_domain' ),
+      'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+      'all_items'             => __( 'All Entries', 'text_domain' ),
+      'add_new_item'          => __( 'Add New Entry', 'text_domain' ),
+      'add_new'               => __( 'Add New', 'text_domain' ),
+      'new_item'              => __( 'New Entry', 'text_domain' ),
+      'edit_item'             => __( 'Edit Entry', 'text_domain' ),
+      'update_item'           => __( 'Update Entry', 'text_domain' ),
+      'view_item'             => __( 'View Entry', 'text_domain' ),
+      'view_items'            => __( 'View Entries', 'text_domain' ),
+      'search_items'          => __( 'Search Entry', 'text_domain' ),
+      'not_found'             => __( 'Not found', 'text_domain' ),
+      'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+      'featured_image'        => __( 'Featured Image', 'text_domain' ),
+      'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+      'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+      'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+      'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+      'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+      'items_list'            => __( 'Entries list', 'text_domain' ),
+      'items_list_navigation' => __( 'Entries list navigation', 'text_domain' ),
+      'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
     );
     $args = array(
-      'labels'                     => $labels,
-      'hierarchical'               => true,
-      'public'                     => true,
-      'show_ui'                    => true,
-      'show_admin_column'          => true,
-      'show_in_nav_menus'          => true,
-      'show_tagcloud'              => true,
-      'show_in_rest'               => true,
+      'label'                 => __( 'Entry', 'text_domain' ),
+      'description'           => __( 'Post Type Description', 'text_domain' ),
+      'labels'                => $labels,
+      'supports'              => array( 'title', 'editor', 'thumbnail' ),
+      'hierarchical'          => false,
+      'public'                => true,
+      'show_ui'               => true,
+      'show_in_menu'          => true,
+      'menu_position'         => 5,
+      'show_in_admin_bar'     => true,
+      'show_in_nav_menus'     => true,
+      'can_export'            => true,
+      'has_archive'           => true,
+      'exclude_from_search'   => false,
+      'publicly_queryable'    => true,
+      'capability_type'       => 'page',
+      'show_in_rest'          => true,
     );
-    register_taxonomy( 'project_year', array( 'post' ), $args );
-  }
-  add_action( 'init', 'project_year_taxonomy', 0 );
+    register_post_type( 'journal_entry', $args );
 
-  // Register Custom Taxonomy ANNO
-  function project_client_taxonomy() {
-
-    $labels = array(
-      'name'                       => _x( 'Client', 'Taxonomy General Name', 'text_domain' ),
-      'singular_name'              => _x( 'Client', 'Taxonomy Singular Name', 'text_domain' ),
-      'menu_name'                  => __( 'Clients', 'text_domain' ),
-      'all_items'                  => __( 'All Items', 'text_domain' ),
-      'parent_item'                => __( 'Parent Item', 'text_domain' ),
-      'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
-      'new_item_name'              => __( 'New Item Name', 'text_domain' ),
-      'add_new_item'               => __( 'Add New Item', 'text_domain' ),
-      'edit_item'                  => __( 'Edit Item', 'text_domain' ),
-      'update_item'                => __( 'Update Item', 'text_domain' ),
-      'view_item'                  => __( 'View Item', 'text_domain' ),
-      'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
-      'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
-      'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
-      'popular_items'              => __( 'Popular Items', 'text_domain' ),
-      'search_items'               => __( 'Search Items', 'text_domain' ),
-      'not_found'                  => __( 'Not Found', 'text_domain' ),
-      'no_terms'                   => __( 'No items', 'text_domain' ),
-      'items_list'                 => __( 'Items list', 'text_domain' ),
-      'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
-    );
-    $args = array(
-      'labels'                     => $labels,
-      'hierarchical'               => true,
-      'public'                     => true,
-      'show_ui'                    => true,
-      'show_admin_column'          => true,
-      'show_in_nav_menus'          => true,
-      'show_tagcloud'              => true,
-      'show_in_rest'               => true,
-    );
-    register_taxonomy( 'project_client', array( 'post' ), $args );
   }
-  add_action( 'init', 'project_client_taxonomy', 0 );
+  add_action( 'init', 'journal_post_type', 0 );
 ?>
