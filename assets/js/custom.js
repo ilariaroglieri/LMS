f856cc2b2f2dee2f49d33d74006f5c34d428ebba
Ilaria Roglieri <ilariaroglieri@gmail.com>
2026-09-23 23:25:13 +0200

    marquee working

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,24 +2,16 @@ document.fonts.ready.then(() => {
 	document.querySelectorAll('.project-title-track').forEach(track => {
 		const span = track.querySelector('.project-title');
 		const titleW = span.getBoundingClientRect().width;
-		const wrapW = track.parentElement.getBoundingClientRect().width;
-		span.style.setProperty('--title-w', titleW + 'px');
+		const vW  = window.innerWidth;
 
-		const totalDist = wrapW + titleW;
-		const duration = 15;
-		const delay = -((wrapW / totalDist) * duration);
-
-		if (titleW >= wrapW) {
+		if (titleW >= vW) {
 			track.classList.add('is-long');
       track.appendChild(span.cloneNode(true));
 		} else {
       track.classList.add('is-short');
-      track.style.height = span.getBoundingClientRect().height + 'px';
 
       const clone = span.cloneNode(true);
-      clone.style.setProperty('--title-w', titleW + 'px');
       clone.classList.add('clone');
-      clone.style.animationDelay = delay + 's';
       track.appendChild(clone);
     }
 	})
